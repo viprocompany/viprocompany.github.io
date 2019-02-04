56f4d06dc92ca88c03da6a3df7136b9cf2a06ab6
viprocompany <vitali_igorevich_doc@mail.ru>
2019-02-04 13:04:14 +0300

    torg start
    просмотр сайта "Торговый объект", без оформленя шрифтов и контента.

---
+++ torg/js/script.js
@@ -0,0 +1,12 @@
+﻿$('.my-carousel').carousel({
+	interval: 7000
+});
+$('.carousel-fade').carousel({
+	interval: 5000
+});
+$(document).ready(function(){
+ $('.footer').width($(window).width());
+})
+$(document).ready(function(){
+ $('.header').width($(window).width());
+})
