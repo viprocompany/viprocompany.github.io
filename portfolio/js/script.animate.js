fccefe720f48f9ecd8ae06547cca6fe707ea325a
viprocompany <vitali_igorevich_doc@mail.ru>
2019-04-05 12:24:46 +0300

    28

--- portfolio/js/script.animate.js
+++ portfolio/js/script.animate.js
@@ -1,16 +1,26 @@
+ // паралаксы 2 штуки
 var $ = jQuery.noConflict();
 $(window).scroll(function() {
 	var st = $(this).scrollTop();
 	// consol.log(st);
 	$('.header-composition').css({
 		"transform" : "translate(0%, " + st/7.7 + "%"
-	})
+	});
+
+	// 		$('.paralax2-body ').css({
+	// 	"transform" : "translate(0%, " + st/20 + "%"
+	// });  // паралакс  фотографии номер 2 между блоками
+	// $('.paralax2-header').css({
+	// 	"transform" : "translate(0%, " + st/20 + "%"
+	// }) // паралакс заголовка над фотографией номер2
 });
 
 $(document).ready(function(){
 	$('.more').hide().show(1200)
 });
 
+
+// var rellax = new Rellax('.rellax');
 // $(document).ready(function(){
 
 // $('.more2').css({'background-color': '#17a2b8'}, 9500);
