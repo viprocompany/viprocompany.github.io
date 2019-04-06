51bf2c2ddb4ffab529f3c7a854532afd366ae8cb
viprocompany <vitali_igorevich_doc@mail.ru>
2019-04-06 23:41:26 +0300

    29
    add page5

---
+++ portfolio/page-5/js/script.js
@@ -0,0 +1,7 @@
+$(".main").tiltedpage_scroll({
+    sectionContainer: "> section",   
+    angle: 50,                       
+    opacity: true,                   
+    scale: true,                     
+    outAnimation: true               
+  })
