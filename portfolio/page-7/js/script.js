5f87b1877f6c631fff0bd32ead93b8058c0551ad
viprocompany <vitali_igorevich_doc@mail.ru>
2019-04-17 13:08:52 +0300

    43

--- portfolio/page-7/js/script.js
+++ portfolio/page-7/js/script.js
@@ -1,7 +1,34 @@
-$(".main").tiltedpage_scroll({
-    sectionContainer: "> section",   
-    angle: 50,                       
-    opacity: true,                   
-    scale: true,                     
-    outAnimation: true               
-  })
+
+    // фон на всю высоту
+$(document).ready(function(){
+ $('.header').height($(window).height());
+});
+
+
+// подключение плагина паралакса BGscroll для фотографии 2(косметика)
+	// $(window).scroll(function(){
+ //  $('.bg--1').bgscroll({
+ //    direction: 'bottom', // направление bottom или top
+ //    bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, 
+ //    // размерность в %, 50 - означает по центру
+ //    debug: false, // Режим отладки
+ //    min:30, // минимальное положение (в %) на которое может смещаться фон
+ //    max:100 // максимальное положение (в %) на которое может смещаться фон
+ //  });
+ //    $('.bg--2').bgscroll({
+ //    direction: 'top',  
+ //  });
+ //  $('.bg--3').bgscroll({
+ //    direction: 'bottom',
+    
+
+ //  });
+ //  $('.bg--4').bgscroll({
+ //    direction: 'top',
+   
+ //  });
+ //    $('.bg--5').bgscroll({
+ //    direction: 'bottom',
+
+ //  });
+// });
